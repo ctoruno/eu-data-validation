e7e4a5bd67253f60dfba18f7d59eea55b5cc261c
Dalia Habiby <habibydalia@gmail.com>
2023-10-16 13:29:46 -0400

    Add pillar 6 to FLE_507

--- TPS/Cleaning/FLE_507/FLE_507_function.R
+++ TPS/Cleaning/FLE_507/FLE_507_function.R
@@ -8,7 +8,7 @@
 ##
 ## Creation date:     October 3rd, 2023
 ##
-## This version:      October 5th, 2023
+## This version:      October 16th, 2023
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
@@ -28,10 +28,11 @@ FLE_507_clean<- function(df){
   
   p1<- c("Q7_6", "Q7_7", "Q7_8", "Q8_1", "Q8_2", "Q8_3")
   p2<- c("Q1_1", "Q3", "Q4_1", "Q4_2", "Q4_3", "Q4_4", "Q4_5", "Q4_6", "Q5_1", "Q5_2", "Q7_2", "Q7_4", "Q7_5")
+  p6<- c("Q1_3", "Q1_4", "Q1_6", "Q1_7", "Q1_8", "Q1_9", "Q4_7")
   
   #"Q6", "Q6R ,"Q7_1"
   
-  targetvars<- c(p1,p2)
+  targetvars<- c(p1,p2, p6)
   
   cntry<- c("AT", "BE", "BG", "CY", "CZ", "DE", "DK", "EE", "EL", "ES", "FI", "FR", "HR", "HU", "IE", "IT", 
             "LT", "LU", "LV", "MT", "NL", "PL", "PT", "RO", "SE", "SI", "SK")
@@ -62,6 +63,22 @@ FLE_507_clean<- function(df){
       vals<-dfq[[1]]
       new_vals<- c(0,1/3,2/3,1, NA_real_)
     }
+    
+    if (n %in% p5){
+      
+      if (n != "Q4_7"){
+        vals<-dfq[[1]]
+        new_vals<- c(0,1/3,2/3,1, NA_real_)
+      }
+      
+      #review for how to code non-existent
+      if (n == "Q4_7"){
+        vals<-dfq[[1]]
+        new_vals<- c(0,0.25,0.5,0.75, 1)
+      }
+      
+    }
+    
       
     
   ## 1.4 Normalize indicators ==================================================================================
