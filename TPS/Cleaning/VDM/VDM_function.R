8f1698e484908f3a01d8206188bf228244d1d08e
Dalia Habiby <habibydalia@gmail.com>
2023-11-07 10:53:21 -0500

    Add expert variables

--- TPS/Cleaning/VDM/VDM_function.R
+++ TPS/Cleaning/VDM/VDM_function.R
@@ -42,7 +42,7 @@ VDM_clean<- function(df){
   
   p7<- c("v2clacjstm", "v2clacjstw")
   
-  val<- c("v2caassemb", "v2jupoatck", "v2exbribe", "v2lgcrrpt", "v2exembez")
+  val<- c("v2caassemb", "v2jupoatck", "v2exbribe", "v2lgcrrpt", "v2exembez", "v2elffelr")
   
   targetvars <- c(p1, p4, p7, val)
 
@@ -69,7 +69,7 @@ VDM_clean<- function(df){
                   "v2csreprss", "v2cltort", "v2clkill", "v2clslavem", "v2clslavef", "v2clprptym", "v2clprptyw", 
                   "v2clrelig", "v2csgender", "v2clacjust", "v2clsocgrp", "v2pepwrgen", "v2pepwrses", "v2pepwrsoc", 
                   "v2clfmove", "v2cldmovem","v2cldmovew", "v2clacjstm", "v2clacjstw", "v2caassemb", "v2jupoatck", 
-                  "v2exbribe", "v2lgcrrpt", "v2exembez")
+                  "v2exbribe", "v2lgcrrpt", "v2exembez", "v2elffelr")
   
   nochange<- setdiff(targetvars, changescale)
   
