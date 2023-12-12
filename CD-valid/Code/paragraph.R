767d3af300b4604685854e36af22edd225d5a133
Dalia Habiby <habibydalia@gmail.com>
2023-12-12 09:59:33 -0500

    Add all country placeholders to paragraph.R

--- CD-valid/Code/paragraph.R
+++ CD-valid/Code/paragraph.R
@@ -1,5 +1,105 @@
 paragraph<- function(country){
   
+  if (country == "Austria"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Belgium"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Bulgaria"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  
+  if (country == "Croatia"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Cyprus"){
+    
+    p<- c("
+    <b>In general, we did not find any significant issues during the data validation process.</b>
+    <br>
+    Some minor issues that we believe are worth noticing are:
+    <br>   
+    <ul> 
+    <ul>    
+    <li>GPP Over Time:         
+    <ul>            
+    <li>We did not find any specific issue when comparing the pretest data to the previous GPP wave in Cyprus. </li>            
+    <li>Since the comparison data is from only 2 years ago, the lack of significant differences is expected.</li>            
+    <li>There is only one red flag in the t-test, regarding paying a bribe to expedite a process (q7e). However, this flag is raised because there is no variation among the pretest observations. </li>            
+    </ul>    
+    </li>
+    <br>
+    <li>TPS Public Opinion Polls:         
+    <ul>            
+    <li>We have found significant differences in comparison to third party public opinion polls. However, this could be attributed to a combination of low matches and/or old data (especially from 2019).</li>            
+    <li>The most significant positive differences were found in subpillars related to independence of judges (q44j_G2) and freedom of media (q39b_G1). Although, the direction of the difference is not consistent among other related indicators. </li>        
+    <li>Some negative differences were found in subpillars related to freedom of CSOs (q44j_G2) and consistently in the cost of justice (q44d_G2 and q44e_G2). We suggest further research regarding the access to justice in Cyprus. </li>
+    </ul>  
+    </li>
+    <br>
+    <li>TPS Expert Data:         
+    <ul>            
+    <li>We found some significant differences in absence of authoritarianism (q53_G1). </li>            
+    <li>There are also many flags in freedom of assembly (q39e_G1) and freedom of speech (q39e_G2). However, these come from differences with Freedom House, whose experts codings for Cyprus reflect a perfect score for the country.</li>            
+    </ul>    
+    </li>
+    </ul>
+    <br>   
+          
+          ")
+    
+  }
+  
+  if (country == "Czechia"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Denmark"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Estonia"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Finland"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "France"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Germany"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  
   if (country == "Greece"){
     
     p<- c( 
@@ -52,9 +152,64 @@ paragraph<- function(country){
     
   }
   
-  if (country == "Cyprus"){
+  if (country == "Hungary"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Ireland"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Italy"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Latvia"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Lithuania"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Luxembourg"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Malta"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Netherlands"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Poland"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+  
+  if (country == "Portugal"){
     
     p<- c("
+    
     <b>In general, we did not find any significant issues during the data validation process.</b>
     <br>
     Some minor issues that we believe are worth noticing are:
@@ -63,75 +218,63 @@ paragraph<- function(country){
     <ul>    
     <li>GPP Over Time:         
     <ul>            
-    <li>We did not find any specific issue when comparing the pretest data to the previous GPP wave in Cyprus. </li>            
-    <li>Since the comparison data is from only 2 years ago, the lack of significant differences is expected.</li>            
-    <li>There is only one red flag in the t-test, regarding paying a bribe to expedite a process (q7e). However, this flag is raised because there is no variation among the pretest observations. </li>            
+    <li>Despite the comparative data being old, distinguishable changes over time were not identified. Consequently, we recommend directing the research focus toward aspects anticipated to have evolved over the years, which are not listed below.</li> 
+    <li>Significant improvements were observed in Media Freedom (q39b_G1), Freedom of Speech (q39e_G2), and Open Government (q3c and q3a).</li>            
+    <li>Contrarily, noteworthy deterioration was found in indicators related to the transparency of Government budget (q9a) and contracts (q9b).</li>            
     </ul>    
     </li>
     <br>
     <li>TPS Public Opinion Polls:         
     <ul>            
-    <li>We have found significant differences in comparison to third party public opinion polls. However, this could be attributed to a combination of low matches and/or old data (especially from 2019).</li>            
-    <li>The most significant positive differences were found in subpillars related to independence of judges (q44j_G2) and freedom of media (q39b_G1). Although, the direction of the difference is not consistent among other related indicators. </li>        
-    <li>Some negative differences were found in subpillars related to freedom of CSOs (q44j_G2) and consistently in the cost of justice (q44d_G2 and q44e_G2). We suggest further research regarding the access to justice in Cyprus. </li>
+    <li>Although some noteworthy distinctions were uncovered, these could potentially be attributed to the outdated data or low matches between GPP questions and TPS indicators.</li>            
+    <li>The pretest data revealed more favorable scores than the TPS comparison indicators in relation to Freedom to Vote (q39d_G1), Media Freedom (q39b_G1), Freedom of Assembly (q39h_G2), and Police Performance (q43e_G2).</li>        
+    <li>Contrarily, the pretest data consistently exhibited lower scores than the TPS comparison indicators concerning Working Conditions (q45c_G1 and q45c_G2), Access to Justice (q44d_G2 and q44e_G2), and the Performance of Prosecutors (q43f_G2).</li>
     </ul>  
     </li>
     <br>
     <li>TPS Expert Data:         
     <ul>            
-    <li>We found some significant differences in absence of authoritarianism (q53_G1). </li>            
-    <li>There are also many flags in freedom of assembly (q39e_G1) and freedom of speech (q39e_G2). However, these come from differences with Freedom House, whose experts codings for Cyprus reflect a perfect score for the country.</li>            
+    <li>Analysis indicates that TPS expert scores are systemically lower than our pretest scores in terms of the Independence of Judges (q44j_G2 and q43_G1), Anti-corruption Government Efforts (q4d), and Equality before the Law (q59a).</li>            
     </ul>    
     </li>
     </ul>
     <br>   
           
-          ")
+    ")
     
   }
   
-  if (country == "Portugal"){
+  if (country == "Romania"){
     
     p<- c("There is no paragraph")
     
   }
   
-  if (country == "Austria"){
+  if (country == "Slovakia"){
     
     p<- c("There is no paragraph")
     
   }
   
-  if (country == "Belgium"){
+  if (country == "Slovenia"){
     
     p<- c("There is no paragraph")
     
   }
   
-  if (country == "Bulgaria"){
+  if (country == "Spain"){
     
     p<- c("There is no paragraph")
     
   }
+  
+  if (country == "Sweden"){
+    
+    p<- c("There is no paragraph")
+    
+  }
+
   
   return(HTML(p))
 }
 
-# <li>Regarding Pillar 4, we have identified large disparities when comparing the pretest data with our own 2017 data and the data from the TPS (Third-Party Source). 
-# Notably, the most significant declines are observed in the areas of media freedom (q39b_G1, q39g_G2), freedom of speech within civil society organizations and political parties (q39h_G2, q39i_G2). 
-# It is essential to note that, in most cases where differences are highlighted, our pretest data indicates more negative perceptions compared to other sources. 
-# Also, please consider that the comparison data is over two years old and there may have been developments in recent years that have affected the perception of fundamental freedoms in the country.
-# Therefore, it's important to explain what these changes are due to. </li>
-#     <br>   
-#     <br>   
-#     <li>For Pillar 5, we have found significant differences in comparison to our 2017 data. 
-#     However, this could be attributed to the time gap between the two data sets. 
-#     Please, explore whether security conditions or perceptions in Greece have deteriorated in recent years. </li>
-#     <br>   
-#     <br>   
-#     <li> Pillars 7 and 8 have shown substantial drops, particularly in subpillars related to the impartiality of judges (q44j_G2). 
-#     Therefore, it is necessary to analyze if any specific factors account for this decline in the perception of the judicial system's efficiency and impartiality. 
-# This is especially crucial when we consider that this analysis reveals extremely divergent results compared to external data in subpillar 1.3 - Independent Oversight. </li>
-#   </ul>
-
-#gsub("[\r\n]", " ", p)
