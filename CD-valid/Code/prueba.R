3a1b3854532a6f18a4d96d8500dfddf8cd1ed9e0
aspardog <92935386+aspardog@users.noreply.github.com>
2024-02-22 11:46:27 -0500

    Prueba tables

--- CD-valid/Code/prueba.R
+++ CD-valid/Code/prueba.R
@@ -12,35 +12,81 @@ countries <- c("Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czechia",
 
 outPath <- paste0(path2eu, "/EU-S Data/eu-data-validation/CD-valid/Outcomes/Pretest/")
 
-countries.ls <- list()
-sheets <- c("time_changes", "tps_comparisson")
-lista_hojas <- list()
-
-for (i in countries) {
-  archivo <- paste0(outPath, i, "/", i, ".xlsx")
-  hojas   <- excel_sheets(archivo)
-  # Iterar sobre cada hoja y leer sus datos
-  for (hoja in hojas) {
-    # Leer los datos de la hoja actual
-    datos_hoja <- read_excel(archivo, sheet = hoja)
-    # Agregar los datos de la hoja a la lista
-    lista_hojas[[paste0(i, "-", hoja)]] <- datos_hoja
-  }
-}
-
-total <- Austria_TC %>% full_join(y = Austria_TPS, by = c("variable" = "GPP_Variable_Name")) %>% distinct(variable)
-
-variables <- codebook.df %>%
+variables_total <- codebook.df %>%
   mutate(filtro = 
            if_else(str_detect(Variable, 
                               "TRT_|ATC_|COR_|ORC_|BRB_|IPR_|IRE_|SEC_|DIS_|AJD_|AJR_|AJE_|CPA_|CPB_|LEP_|CJP_|CTZ_|PAB_|JSE_|ROL_"), 
                    1, 0)
   ) %>%
-  filter(filtro == 1)
+  filter(filtro == 1) %>%
+  nrow()
 
-"%!in%" <- compose("!", "%in%")
-
-variables_wa2j <- variables %>% 
+variables_wa2j <- codebook.df %>% 
   filter(str_detect(Variable, 
                     "TRT_|ATC_|COR_|ORC_|BRB_|IPR_|IRE_|SEC_|DIS_|CPA_|CPB_|LEP_|CJP_|CTZ_|PAB_|JSE_|ROL_")) %>%
-  filter(!str_detect(Variable, "DIS_exp"))
+  filter(!str_detect(Variable, "DIS_exp")) %>%
+  nrow()
+
+variables_report <- codebook.df %>%
+  filter(Report == 1) %>%
+  nrow()
+
+# Initialize a list to store the results for each country
+variables_checked <- vector("list", length(countries))
+integrated_data <- list()
+# Iterate over each country
+for (i in seq_along(countries)) {
+  # Initialize an empty list to store datasets for each sheet
+  country_data <- list()
+  
+  archivo <- paste0(outPath, countries[i], "/", countries[i], ".xlsx")
+  hojas   <- excel_sheets(archivo)
+  
+  # Iterating over each sheet
+  for (hoja in hojas) {
+    # Leer los datos de la hoja actual
+    datos_hoja <- read_excel(archivo, sheet = hoja)
+    # Store the dataset in the list with the sheet name as key
+    country_data[[hoja]] <- datos_hoja %>% 
+      drop_na()
+  }
+  
+  # Store the list of datasets for the current country in the integrated_data list
+  integrated_data[[countries[i]]] <- country_data
+  
+  # Perform the operation for each country and store the result
+  variables_checked[[i]] <- integrated_data[[countries[i]]]$time_changes %>%
+    full_join(integrated_data[[countries[i]]]$tps_comparisson,
+              by = c("variable" = "GPP_Variable_Name")) %>%
+    distinct(variable) %>%
+    nrow()
+}
+
+# Print the results
+names(variables_checked) <- countries
+variables_checked
+
+# Calculate total ratio for each country and bind the results
+total_vars <- sapply(countries, function(country) {
+  cbind(variables_checked[[country]])
+})
+
+# Calculate total ratio for each country and bind the results
+total_ratios <- sapply(countries, function(country) {
+  cbind(variables_checked[[country]]) / variables_total
+})
+
+total_ratios_wa2j <- sapply(countries, function(country) {
+  cbind(variables_checked[[country]]) / variables_wa2j
+})
+
+total_ratios_report <- sapply(countries, function(country) {
+  cbind(variables_checked[[country]]) / variables_report
+})
+
+tableCountry <- tibble(country = countries,
+                       total_vars = total_vars,
+                       total_vars_prop = total_ratios,
+                       total_vars_wo_a2j_prop = total_ratios_wa2j,
+                       total_vars_report = total_ratios_report)
+write_xlsx(tableCountry, path = "Outcomes/Pretest/overviewCountry.xlsx")
