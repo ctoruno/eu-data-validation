eed7bb6184fae076931036a548e19b8cc613e1ae
Dalia Habiby <habibydalia@gmail.com>
2024-03-05 09:24:56 -0500

    Update outlier_analysis.R

--- CD-valid/Code/outlier_analysis.R
+++ CD-valid/Code/outlier_analysis.R
@@ -8,7 +8,7 @@
 ##
 ## Creation date:     March 4th, 2024
 ##
-## This version:      March 4th, 2024
+## This version:      March 5th, 2024
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
@@ -16,4 +16,51 @@
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-outlier_analysis<- function(gpp_data.df = master_data.df)
+outlier_analysis<- function(gpp_data.df = fullmerge){
+  
+  ## 1.1 Identifying indicators    =============================================================================
+  countries<- unique(gpp_data.df$country_name_ltn)
+  vars<- colnames(select_if(gpp_data.df, is.numeric))[-c(1:5)]
+  
+  ## 1.2 Sub-setting data  =====================================================================================
+  
+  oriented<- gpp_data.df
+  
+  results<- data.frame(matrix(nrow = 0, ncol = 3))
+  colnames(results)<- c("Country", "Question", "Flag")
+  
+  for (i in vars){
+    
+    oriented[[i]]<- ifelse(oriented[[i]] %in% c(98,99, 8888, 9999), NA_real_, oriented[[i]])
+    
+    mean_agg<- mean(oriented[[i]], na.rm = TRUE)
+    sd_agg<- sd(oriented[[i]], na.rm = TRUE)
+    bound<- 2.5*sd_agg
+    
+    if (!is.na(sd_agg) & !is.na(mean_agg)){
+      for (j in countries){
+        
+        country_df<- oriented%>%
+          filter(country_name_ltn == j)
+        
+        if (sum(country_df[[i]], na.rm = TRUE)!= 0){
+          
+          mean_cy<- mean(country_df[[i]], na.rm = TRUE)
+          
+          if (abs(mean_agg-mean_cy)> bound){
+            
+            results[nrow(results)+1]<- c(j, i, "Red Flag")
+          } else {
+            results[nrow(results)+1,]<- c(j, i, "Green Flag")
+          }
+          
+        }
+        
+      }
+    }
+  }
+  
+  return(results)
+  
+}
+
