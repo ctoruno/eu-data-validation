51f7b506f48038a27aab9ca37cdbc423e009696e
Dalia Habiby <habibydalia@gmail.com>
2024-03-15 16:41:09 -0400

    Complete flagging system

--- CD-valid/Code/flagging_system.R
+++ CD-valid/Code/flagging_system.R
@@ -18,10 +18,6 @@
 
 flagging_system<- function(gpp_data.df = fullmerge){
   
-  reportvars<- codebook.df %>%
-    filter(Report == 1)
-  
-  reportvarslist<- reportvars$Variable
   ## 1.1 Internal Flags  =====================================================================================
 
   over_time<- data.frame()
@@ -31,7 +27,7 @@ flagging_system<- function(gpp_data.df = fullmerge){
       filter(country_name_ltn == i)
     
     time_changes.df <- time_changes(data.df = df,
-                                    gpp_vars = reportvarslist,
+                                    gppvars = reportvarslist,
                                     country = i, 
                                     type = "full")
     
@@ -40,21 +36,70 @@ flagging_system<- function(gpp_data.df = fullmerge){
   
   internal<- over_time%>%
     select(country, variable, warning)%>%
-    rename("Over_time"= warning)
+    rename("time"= warning)
+  
+  internal$time<- ifelse(internal$time == "Not enough info", NA_character_, internal$time)
   
   internal<- left_join(outlier_analysis.df%>%
-    rename("Outliers_analysis" = Flag), internal, by = join_by("Country" == "country", "Question" == "variable"))
+    rename("outliers" = Flag), internal, by = join_by("Country" == "country", "Question" == "variable"))
   
+  internal$outliers<- ifelse(internal$outliers == "Not Enough Info", NA_character_, internal$outliers)
 
 
   ## 1.2 External Flags  =====================================================================================
   
-  TPS_ranking_analysis.df%>%
-    filter(Type_Survey== "population")
+  ranking<- TPS_ranking_analysis.df%>%
+    select(Pillar, country_name_ltn, question, flagged_questions, flagged_score, Type_Survey)%>%
+    distinct()%>%
+    pivot_wider(names_from = Type_Survey, values_from = c(flagged_questions, flagged_score), values_fn = list)%>% 
+    unnest(cols = everything())%>%
+    select(-flagged_score_expert)
   
+  hm<- left_join(internal, ranking, by = join_by("Country" == "country_name_ltn", "Question" == "question"))%>%
+    distinct()%>%
+    rename("p_ranking" = flagged_questions_population,
+           "threshold" = flagged_score_population,
+           "e_ranking" = flagged_questions_expert)
   
-  final_flags<- data.frame(matrix(nrow= 0, ncol = 3))
+  hm[,c("outliers", "time", "p_ranking", "e_ranking", "threshold")] <- lapply(hm[,c("outliers", "time", "p_ranking", "e_ranking", "threshold")], gsub, pattern = "\\s.*", replacement = "")
   
+  final_flags<- hm%>%
+    mutate("External_validation" = ifelse(is.na(p_ranking) & is.na(e_ranking) & is.na(threshold), NA_character_,
+                                    ifelse(is.na(p_ranking)& is.na(threshold) & e_ranking %in% c("Green", "Red"), paste0(e_ranking, "*"),
+                                    ifelse(is.na(p_ranking)& is.na(threshold) & e_ranking == "Yellow", "Green*",
+                                    ifelse(p_ranking == "Red", "Red", 
+                                    ifelse(e_ranking == "Red" & threshold == "Red", "Red", 
+                                    ifelse(p_ranking == "Yellow" & e_ranking == "Yellow" & threshold == "Yellow", "Red", "Green")))))),
+           
+           "Internal_validation" = ifelse(is.na(outliers) & is.na(time), NA_character_,
+                                    ifelse(is.na(outliers), paste0(time, "*"),
+                                    ifelse(is.na(time), paste0(outliers, "*"),
+                                    ifelse(outliers == "Red" & time == "Red", "Red", 
+                                    ifelse(outliers == "Green" & time == "Green", "Green",
+                                    ifelse(outliers == "Green" & time == "Red", "Red",
+                                    ifelse(outliers == "Red" & time == "Green", "Red", NA_character_))))))),
+           
+           "Final_flag" = ifelse(is.na(External_validation), paste0(Internal_validation, "*"),
+                           ifelse(is.na(Internal_validation), paste0(External_validation, "*"),
+                           ifelse(External_validation == "Red" & Internal_validation == "Red", "Red", 
+                           ifelse(External_validation == "Red" & Internal_validation == "Green", "Yellow",
+                           ifelse(External_validation == "Green" & Internal_validation == "Red", "Yellow",
+                           ifelse(External_validation == "Green" & Internal_validation == "Green", "Green", 
+                           ifelse(grepl("Red", External_validation) & Internal_validation == "Red", "Red**",
+                           ifelse(grepl("Red", External_validation) & Internal_validation == "Green", "Yellow**",
+                           ifelse(grepl("Green", External_validation) & Internal_validation == "Red", "Yellow**",
+                           ifelse(grepl("Green", External_validation) & Internal_validation == "Green", "Green**", 
+                           ifelse(grepl("Red", Internal_validation) & External_validation == "Red", "Red**",
+                           ifelse(grepl("Red", Internal_validation) & External_validation == "Green", "Yellow**",
+                           ifelse(grepl("Green", Internal_validation) & External_validation == "Red", "Yellow**",
+                           ifelse(grepl("Green", Internal_validation) & External_validation == "Green", "Green**",
+                           ifelse(grepl("Red", Internal_validation) & grepl("Red", External_validation), "Red***",
+                           ifelse(grepl("Red", Internal_validation) & grepl("Green", External_validation), "Yellow***",
+                           ifelse(grepl("Green", Internal_validation) & grepl("Red", External_validation), "Yellow***",
+                           ifelse(grepl("Green", Internal_validation) & grepl("Green", External_validation), "Green***", NA_character_))))))))))))))))))
+           )
   
+  return(final_flags)
+
   
 }
