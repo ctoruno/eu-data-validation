e612799baa0ee2ec416e45874a4da0bc8472dafe
Dalia Habiby <habibydalia@gmail.com>
2024-04-17 10:30:01 -0400

    add new kable creation function and refactor HTML template code

---
+++ CD-valid/Code/kable_function.R
@@ -0,0 +1,32 @@
+kable_function<- function(data, type){
+  if (nrow(data)>0){
+    
+    if (type == "gpp"){
+      colnums<- c(6,7,8,9,10)
+    } else if (type == "tps"){
+      colnums<- c(9, 10, 11, 12, 13)
+    }
+    
+    knitr::kable(data, caption = "Variable List and Flags", booktabs = F, escape = F, digits=3)%>%
+      kable_classic(html_font = "Arial")%>%
+      kable_styling(full_width = F)%>%
+      row_spec(0, bold= TRUE)%>%
+      row_spec(which(data$Flag == "Red"), background = "#FC7661", extra_css = "border-bottom: 1px solid;")%>%
+      row_spec(which(data$Flag == "Green"), background = "#B6F161", extra_css = "border-bottom: 1px solid;")%>%
+      row_spec(which(data$Flag == "Yellow"), background = "#FFFD69", extra_css = "border-bottom: 1px solid;")%>%
+      column_spec(colnums, bold= TRUE)%>%
+      remove_column(3)%>%
+      scroll_box(width = "1000px", height = "500px", fixed_thead = T)
+  } else {
+    
+    data<- data%>%
+      select(-Flag)
+    
+    knitr::kable(data, caption = "Variable List and Flags", booktabs = F, escape = F, digits=3)%>%
+      kable_classic(html_font = "Arial", )%>%
+      kable_styling(bootstrap_options = c("striped"), full_width = T)%>%
+      row_spec(0, bold= TRUE)%>%
+      scroll_box(width = "1000px", height = "500px", fixed_thead = T)
+  }
+  
+}
