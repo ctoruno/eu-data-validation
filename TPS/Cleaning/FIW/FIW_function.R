62bd8658d28e760022dadb461bf4998c703491bb
Noah Clapacs <nclapacs@worldjusticeproject.org>
2024-05-06 12:38:38 -0400

    Update FIW_function.R

--- TPS/Cleaning/FIW/FIW_function.R
+++ TPS/Cleaning/FIW/FIW_function.R
@@ -28,7 +28,7 @@ FIW_clean <- function(df){
 
   ## 1.1 Identifying indicators  ===============================================================================
   
-  targetvars <- c("Country/Territory", "A", "A1", "A2", "A3", "B", "B1", "B2", "B4", "C2", "D", "D1", "D2", "D4", "E",
+  targetvars <- c("Country/Territory", "A", "A1", "A2", "A3", "B", "B1", "B2", "B4", "C2", "C3", "D", "D1", "D2", "D4", "E",
                   "E1", "E2", "E3", "F", "F1", "F2", "F4", "PR", "CL", "G1", "G2")
   
   
