3e76f58125ebc9b5499ea21961d268ab78322050
Dalia Habiby <habibydalia@gmail.com>
2024-06-03 11:58:12 -0400

    create NUTS level functions

---
+++ ALL-valid/Code/nuts_html_flags.R
@@ -0,0 +1,36 @@
+nuts_html_flags<- function(){
+  
+  
+  subp<- metadata%>%
+    select(GPP_Variable_Name = DAU_GPP, Question, subpillar = sub_pillar) %>%
+    arrange(subpillar) %>%
+    left_join(QRQ_description %>% select(pillar, pillar_name, pillar_id, subpillar, subpillar_name),
+              by = "subpillar") %>%
+    distinct() %>%
+    select(GPP_Variable_Name, Sub_Pillar = subpillar, Pillar = pillar)%>%
+    rbind(variable_list.df%>%
+            select(variable, subpillar, pillar)%>%
+            rename("GPP_Variable_Name" = "variable", 
+                   "Sub_Pillar" = "subpillar",
+                   "Pillar" = "pillar"))%>%
+    distinct()
+  
+  df<- data.frame(matrix(nrow = 0, ncol = 4))
+  colnames(df)<- c("Country", "GPP_Variable_Name", "Sub_Pillar", "Pillar")
+  for (i in 1:length(countrylist)){
+    
+    par<- paragraph(countrylist[i], type = "full")
+    spmatches <- str_extract_all(par, "[0-9]\\.[0-9]{1,2}:")
+    spmatches <- unlist(spmatches)
+    
+    if (length(spmatches) >0){
+      vars<- subp[grepl(paste(spmatches, collapse="|"), subp$Sub_Pillar), ]
+      vars2<- vars%>%
+        mutate(Country = countrylist[i])%>%
+        select(Country, everything())
+      
+      df<- rbind(df, vars2)
+    }
+  }
+  return(df)
+}
