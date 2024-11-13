7891dd9b6909625adbf2c874c75dfea157522f61
aspardog <92935386+aspardog@users.noreply.github.com>
2024-11-13 07:44:53 -0500

    RunMe updated

--- ALL-valid/RunMe.R
+++ ALL-valid/RunMe.R
@@ -175,7 +175,7 @@ eu_qrq_final_scores <- read_dta(paste0(path2eu,
   )
 
 extreme_scores_positive <- eu_qrq_final_scores %>%
-  filter(QRQ_value > 0.99) %>%
+  filter(QRQ_value > 0.95) %>%
   arrange(indicator, -QRQ_value)  %>%
   filter(indicator %!in% c("p_1","p_2","p_3","p_4", "p_5","p_6", "p_7", "p_8"))
 
